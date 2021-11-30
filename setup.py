3bb984599fcd4db787d1987ceb5b0229319891e9
mskinner5278 <root@localhost.localdomain>
2021-11-30 07:22:43 -0800

    Registered handler in setup

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
     packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
-        'console_scripts': [
-            # 'command = some.module:some_function',
+        "databroker.handlers": [
+            "AD_PILATUS_MX = nyxtools.handlers:PilatusHandlerMX",
         ],
     },
     include_package_data=True,
