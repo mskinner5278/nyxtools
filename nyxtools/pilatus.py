15f00011682b835e8b51cae2573b2701f6bc5d46
Jun Aishima <jaishima@bnl.gov>
2021-12-01 10:22:14 -0500

    fix isort issue

--- nyxtools/pilatus.py
+++ nyxtools/pilatus.py
@@ -1,10 +1,8 @@
 # import datetime
 
-from ophyd import Component as Cpt
-
 # from ophyd import Device, EpicsPathSignal, EpicsSignal, ImagePlugin, Signal
+from ophyd import Component as Cpt
 from ophyd import Device, ImagePlugin
-
 # from ophyd.areadetector.base import ADComponent
 from ophyd.areadetector.detectors import PilatusDetector
 from ophyd.areadetector.filestore_mixins import FileStoreBase
